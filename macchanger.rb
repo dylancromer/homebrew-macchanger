7e52045408838143ce8863fe550168b436551f1b
dylancromer <dylanmattc@gmail.com>
2019-11-20 09:28:36 -0500

    Pointed homebrew formula at me

--- macchanger.rb
+++ macchanger.rb
@@ -1,9 +1,9 @@
 require "formula"
 
 class Macchanger < Formula
-  homepage "https://github.com/acrogenesis/macchanger"
+  homepage "https://github.com/dylancromer/macchanger"
   version "1.3.2"
-  url "https://github.com/acrogenesis/macchanger/archive/v1.3.2.tar.gz"
+  url "https://github.com/dylancromer/macchanger/archive/v1.3.2.tar.gz"
   sha256 "27ef462e6726305ccbc760c78b26c4d7fa47a49047b204950402f7c91a377605"
 
   def install
